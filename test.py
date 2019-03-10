cce5a8fa9ce51b158e4cb68f8eb6a069e97736e3
thekuhaku <thekuhaku25@gmail.com>
2019-03-10 12:41:51 -0400

    added documentation test.py

--- test.py
+++ test.py
@@ -1,18 +1,22 @@
 import smtplib
 
+# s.sendmail(from, to, msg)
+username = "user069152"
+password = "pass069152"
+msg = "Hello, this is a test"
+
 # connect to server
 s = smtplib.SMTP(host='smtp.gmail.com', port=587)
 
 # encrypt
 s.starttls()
 
-username = "user069152"
-password = "pass069152"
-msg = "Hello, this is a test"
-
 # login
 s.login(username, password)
 
+
+
+# spammer
 counter = 0
 while counter < 50:
     s.sendmail(username + "@gmail.com", username + "@gmail.com", msg)
